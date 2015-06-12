981c82cad7effd1c8bb84ded0c1cc04f204e74cb
voldemortensen <jmort47@hotmail.com>
2015-06-12 18:12:25 +0000

    Add base for WP_Customize_Menus unit tests

---
+++ tests/test-class-wp-customize-menus.php
@@ -0,0 +1,46 @@
+<?php
+
+class Test_WP_Customize_Menus extends WP_UnitTestCase {
+
+	/**
+	 * Instance of WP_Customize_Manager which is reset for each test.
+	 *
+	 * @var WP_Customize_Manager
+	 */
+	public $wp_customize;
+
+	/**
+	 * Set up a test case.
+	 *
+	 * @see WP_UnitTestCase::setup()
+	 */
+	function setUp() {
+		parent::setUp();
+		require_once ABSPATH . WPINC . '/class-wp-customize-manager.php';
+		wp_set_current_user( $this->factory->user->create( array( 'role' => 'administrator' ) ) );
+		global $wp_customize;
+		$this->wp_customize = new WP_Customize_Manager();
+		$wp_customize = $this->wp_customize;
+	}
+
+	/**
+	 * Delete the $wp_customize global when cleaning up scope.
+	 */
+	function clean_up_global_scope() {
+		global $wp_customize;
+		$wp_customize = null;
+		parent::clean_up_global_scope();
+	}
+
+	/**
+	 * Test constructor.
+	 *
+	 * @see WP_Customize_Nav_Menu_Setting::__construct()
+	*/
+	function test_construct() {
+		do_action( 'customize_register', $this->wp_customize );
+		$menus = new WP_Customize_Menus( $this->wp_customize );
+		$this->assertInstanceOf( 'WP_Customize_Menus', $menus->manager );
+	}
+
+}
